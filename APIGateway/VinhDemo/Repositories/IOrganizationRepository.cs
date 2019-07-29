f1b09ebb3116465fb5d58dcb2ca8c135767cd893
PhungVinh <44052557+PhungVinh@users.noreply.github.com>
2019-07-29 17:30:08 +0700

    vinhnp 29/07/2019
    vinhnp 29/07/2019

---
+++ APIGateway/VinhDemo/Repositories/IOrganizationRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+//using OrganizationManagement.EmailService;
+using OrganizationManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrganizationManagement.Repositories
+{
+    public interface IOrganizationRepository 
+    {
+        object GetOrganizationList(string strFilter);
+        object GetPosition(string strFilter);
+        object GetListLogUser();
+        object GetOrganization();
+ 
+        string GetStringCache(string cacheKey);
+        void SetStringCache(string cacheKey, Object obj);
+     
+      
+
+        // Vinh
+        int AddlogUser(TblLogUser logUser);
+        int UpdateLogUser(TblLogUser logUser);
+        int DeleteLogUser(int id);
+        object GetLogUserList(string OrganizationCode);
+        object SearchLogUser(string textSeach, int currentPage, int recordPerPage);
+        object SearchCategory(string textSearch, int currentPage, int recordPerPage);
+        object SearchUserVinhDemo(string textSearch, int currPage, int recordperpage);        
+    }
+}
